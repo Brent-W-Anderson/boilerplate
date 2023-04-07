17253b9747649e69d0f38ee686bdcd3fc1272050
J-A-R-V-I-S\gobre <gobrent21@gmail.com>
2023-04-06 22:00:21 -0500

    add React.StrictMode for debugging within the devServer

--- client/index.tsx
+++ client/index.tsx
@@ -6,8 +6,20 @@ const Routes = loadable( () => import( './pages/routes' ) )
 
 const container = document.getElementById( 'root' )
 
+const App = () => {
+    if ( process.env.NODE_ENV === 'development' ) {
+        return (
+            <React.StrictMode>
+                <Routes />
+            </React.StrictMode>
+        )
+    }
+
+    return <Routes />
+}
+
 if ( container ) {
-    ReactDOM.hydrateRoot( container, <Routes /> )
+    ReactDOM.hydrateRoot( container, <App /> )
 } else {
     console.error( 'Failed to find the root element to mount the application.' )
 }
